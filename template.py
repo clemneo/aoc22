9810de24985b7522b2851992cc6567cd47fcbdb4
clemneo <clement@clementneo.com>
2022-12-01 23:24:12 +0000

    Added template

---
+++ template.py
@@ -0,0 +1,23 @@
+import pprint
+pp = pprint.PrettyPrinter()
+
+def open_file(file):
+  with open(file, 'r+') as f:
+    lines = f.readlines()
+  output_lines = []
+  for line in lines:
+    output_lines.append(line.strip())
+  return output_lines
+
+def solver(part):
+  input_file = open_file('input/day01.txt')
+
+  if part == 1:
+    return None
+  if part == 2:
+    return None
+
+
+
+print("Part 1: ", solver(1))
+print("Part 2: ", solver(2))
